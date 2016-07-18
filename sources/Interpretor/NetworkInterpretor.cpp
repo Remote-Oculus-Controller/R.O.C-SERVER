914302ec3aa251c3f0e5be83516b0ae2398dd431
Hector Roussille <hector.roussille@gmail.com>
2016-07-18 16:07:57 +0200

    added log id on receive

--- sources/Interpretor/NetworkInterpretor.cpp
+++ sources/Interpretor/NetworkInterpretor.cpp
@@ -85,6 +85,7 @@ unsigned NetworkInterpretor::createHeader(unsigned type , unsigned from , unsign
 
 void NetworkInterpretor::handlePacket(protocol::Packet * message)
 {
+ logger::log("id : " + std::to_string(message->id()) , logger::logType::WARNING);
   switch (message->id()) {
     case 0x30:
       this->connectionQuery(message);
