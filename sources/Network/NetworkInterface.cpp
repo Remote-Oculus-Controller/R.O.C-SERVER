dddc26e336b96f451a24506f188e43923be28d6a
Hector Roussille <hector.roussille@gmail.com>
2016-07-20 21:52:26 +0200

    further investigations on crash while executing commands

--- sources/Network/NetworkInterface.cpp
+++ sources/Network/NetworkInterface.cpp
@@ -9,12 +9,13 @@ protocol::Packet * NetworkInterface::get(char * buffer , size_t size)
   char * copy = new char[size];
   memcpy(copy , buffer , size);
   protocol::Packet * elem = new protocol::Packet();
-  ArrayInputStream ais(copy  ,size);
+  ArrayInputStream ais(copy ,size);
   CodedInputStream coded_input(&ais);
   CodedInputStream::Limit msgLimit = coded_input.PushLimit(size);
   if (elem->ParseFromCodedStream(&coded_input))
   {
      coded_input.PopLimit(msgLimit);
+     std::cout << "MESSAGE RECEIVE ID : " << elem->id() << std::endl;
      return elem;
   }
   coded_input.PopLimit(msgLimit);
